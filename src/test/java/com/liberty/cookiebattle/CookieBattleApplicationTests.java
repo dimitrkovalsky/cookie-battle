70c6579e8e40cd0ad738a7347c5fc7472b79fd4f
dkovalskyi <dkovalskyi@playtika>
2017-05-12 14:25:49 +0300

    Initial commit

---
+++ src/test/java/com/liberty/cookiebattle/CookieBattleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.liberty.cookiebattle;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CookieBattleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
