6633e56fb802440f0585af7fb0f405365ce370ed
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-05-19 10:42:26 +0300

    remove dependent links

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -175,9 +175,8 @@ Game.Launch=function()
 		
 		Game.version=1.036;
 		Game.beta=0;
-		l('versionNumber').innerHTML='v.'+Game.version+(Game.beta?' <span style="color:#ff0;">beta</span>':'');
+//		l('versionNumber').innerHTML='v.'+Game.version+(Game.beta?' <span style="color:#ff0;">beta</span>':'');
 		//l('links').innerHTML=(Game.beta?'<a href="../" target="blank">Live version</a> | ':'<a href="beta" target="blank">Try the beta!</a> | ')+'<a href="http://orteil.dashnet.org/experiments/cookie/" target="blank">Cookie Clicker Classic</a>';
-		l('links').innerHTML='<a href="http://orteil.dashnet.org/experiments/cookie/" target="blank">Cookie Clicker Classic</a>';
 		
 		//latency compensator stuff
 		Game.time=new Date().getTime();
